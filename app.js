3b068d7a2bd7a51df78738ddbbd8ad53d54b2975
Noi Narisak <noi.narisak@gmail.com>
2016-06-14 16:08:41 +0000

    format app.js for readability

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var express = require('express'),
  */
 dotenv.load({ path: '.env.example' });
 
+/**
+ * Connect to MongoDB 
+ */
 var db;
 if (process.env.ENV == 'Test') {
     db = mongoose.connect(process.env.MONGODB_URI_TEST);
@@ -20,21 +23,28 @@ mongoose.connection.on('error', () => {
   process.exit(1);
 });
 
-var Book = require('./models/bookModel');
-
+/**
+ * Setup ExpressJS
+ */
 var app = express();
 var port = process.env.PORT || 3000;
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+/**
+ * Setup Mongoose Model(s) and Route(s)
+ */
+var Book = require('./models/bookModel');
 var bookRouter = require('./routes/bookRoutes')(Book);
-
 app.use('/api/books', bookRouter);
 
+/**
+ * Handle the root GET call
+ */
 app.get('/', function(req, res) {
    res.send('Welcome to my API.');
 });
+
 
 app.listen(port, function() {
     console.log('Now it changed to something else. Running on PORT:' + port);
