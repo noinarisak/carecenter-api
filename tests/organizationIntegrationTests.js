7016f74784fa32943a352166c0b26253adbda8b8
Noi Narisak <noi.narisak@gmail.com>
2016-07-29 21:07:53 +0000

    update schema for org and services model

--- tests/organizationIntegrationTests.js
+++ tests/organizationIntegrationTests.js
@@ -4,27 +4,29 @@ var should = require('should'),
     mongoose = require('mongoose'),
     Organization = mongoose.model('Organization'),
     agent = request.agent(app);
-    
+
 describe('Organization Crud Test', function(){
     var organizationPost;
     var organizationPut;
     var organizationPatch;
     var organizationId;
-    
+
     before(function(done){
         organizationPost = {
-            m_id: 0, 
-            name:'Nicasa', 
-            description:'Mental Heath Services Dupage County'};
+            m_id: 0,
+            name:'Nicasa',
+            description:'Mental Heath Services Dupage County',
+            web_url: "http://www.mentalheath.org/"
+        };
         organizationPut = {
-            m_id: 0, 
-            name:'Nicasa', 
+            m_id: 0,
+            name:'Nicasa',
             description:'Mental Heath Services Dupage & Cook County'};
         organizationPatch = {enable:false};
-        
+
         done();
     })
-    
+
     it('POST /organization', function(done){
         agent.post('/api/organizations')
             .send(organizationPost)
@@ -34,17 +36,19 @@ describe('Organization Crud Test', function(){
                 results.body.m_id.should.equal(0);
                 results.body.should.have.property('description').which.is.a.String();
                 results.body.should.have.property('_id');
+                results.body.should.have.property('web_url').which.is.a.String();
                 results.body.should.have.property('services').which.is.a.Array();
+                results.body.should.have.property('tags').which.is.a.Array();
                 organizationId = results.body._id;
-               
+
                 // console.log(results.body);
                 done();
             })
     })
-      
+
     it('GET /organizations/:organizationId', function(done) {
         if (!organizationId) return console.error("organizationId is %s", "Empty or Nil");
-        
+
         agent.get('/api/organizations/' + organizationId)
             .expect(200)
             .end(function(err, results){
@@ -56,13 +60,14 @@ describe('Organization Crud Test', function(){
                 results.body.should.have.property('description').which.is.a.String();
                 results.body.description.should.equal(organizationPost.description);
                 results.body.should.have.property('_id');
+                results.body.should.have.property('web_url').which.is.a.String();
                 results.body.should.have.property('services').which.is.a.Array();
-                
+                results.body.should.have.property('tags').which.is.a.Array();
                 // console.log(results.body);
                 done();
             })
     })
-    
+
     // it('GET /organizations/?genre=', function(done) {
     //     agent.get('/api/organizations/?genre=Fiction')
     //         .expect(200)
@@ -71,15 +76,15 @@ describe('Organization Crud Test', function(){
     //             results.body[0].genre.should.equal('Fiction');
     //             results.body[0].title.should.equal('New Book');
     //             results.body[0].author.should.equal('Noi');
-                
+
     //             // console.log(results.body);
     //             done();
     //         })
     // })
-    
+
     // it('PUT /organizations/:organizationId', function(done) {
     //     console.log('PUT: '+ organizationId)
-        
+
     //     agent.put('/api/organizations/' + organizationId)
     //         .send(organizationPut)
     //         .expect(200)
@@ -88,14 +93,14 @@ describe('Organization Crud Test', function(){
     //             results.body.genre.should.equal('Science Fiction');
     //             results.body.title.should.equal('New Book');
     //             results.body.author.should.equal('John Doe');
-                
+
     //             done();
     //         })
     // })
-    
+
     // it('PATCH /organizations/:organizationId', function(done) {
     //     console.log('PATCH: '+ organizationId)
-        
+
     //     agent.patch('/api/organizations/' + organizationId)
     //         .send(organizationPatch)
     //         .expect(200)
@@ -105,33 +110,33 @@ describe('Organization Crud Test', function(){
     //             results.body.title.should.equal('New Book');
     //             results.body.author.should.equal('John Doe');
     //             results.body.read.should.equal(true);
-                
+
     //             // console.log(results.body);
     //             done();
     //         })
     // })
-    
+
     // it('DELETE /organizations/:organizationId', function(done) {
     //     console.log('DELETE: '+ organizationId)
-        
+
     //     agent.del('/api/organizations/' + organizationId)
     //         .expect(200)
     //         .end(function(err, results){
     //             results.body.message.should.equal('Successful delete');
-                
+
     //             // console.log(results.body);
     //             done();
     //         })
     // })
-    
+
     // afterEach(function(done){
     //   Book.remove().exec();
     //   done();
     // })
-    
+
     after(function(done) {
         Organization.remove({}).exec();
         done();
     })
-    
+
 });
