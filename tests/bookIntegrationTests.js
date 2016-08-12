1ade00af259003b53ca46d2ff6d23c726f1b4f49
Noi Narisak <noi.narisak@gmail.com>
2016-08-12 21:59:59 +0000

    Revert "add failure to test out travis.ci"
    This reverts commit 5e04f6eff33284002b81df7f74247508c3e40b2b.

--- tests/bookIntegrationTests.js
+++ tests/bookIntegrationTests.js
@@ -4,21 +4,21 @@ var should = require('should'),
     mongoose = require('mongoose'),
     Book = mongoose.model('Book'),
     agent = request.agent(app);
-
+    
 describe('Book Crud Test', function(){
     var bookPost;
     var bookPut;
     var bookPatch;
-    var bookId;
-
+    var bookId; 
+    
     before(function(done){
         bookPost = {title:'New Book', author:'Noi', genre:'Fiction'};
         bookPut = {title:'New Book', author:'John Doe', genre:'Science Fiction'};
         bookPatch = {read:true};
-
+        
         done();
     })
-
+    
     it('POST /books', function(done){
         agent.post('/api/books')
             .send(bookPost)
@@ -28,12 +28,12 @@ describe('Book Crud Test', function(){
                 results.body.read.should.equal(false);
                 results.body.should.have.property('_id');
                 bookId = results.body._id;
-
+                
                 // console.log(results.body);
                 done();
             })
     })
-
+      
     it('GET /books/:bookId', function(done) {
         agent.get('/api/books/' + bookId)
             .expect(200)
@@ -43,14 +43,14 @@ describe('Book Crud Test', function(){
                 results.body.genre.should.equal('Fiction');
                 results.body.title.should.equal('New Book');
                 results.body.author.should.equal('Noi');
-
+                
                 // results.body.should.be.instanceof(Array);
-
+                
                 // console.log(results.body);
                 done();
             })
     })
-
+    
     it('GET /books/?genre=', function(done) {
         agent.get('/api/books/?genre=Fiction')
             .expect(200)
@@ -59,15 +59,15 @@ describe('Book Crud Test', function(){
                 results.body[0].genre.should.equal('Fiction');
                 results.body[0].title.should.equal('New Book');
                 results.body[0].author.should.equal('Noi');
-
+                
                 // console.log(results.body);
                 done();
             })
     })
-
+    
     it('PUT /books/:bookId', function(done) {
         // console.log('PUT: '+ bookId)
-
+        
         agent.put('/api/books/' + bookId)
             .send(bookPut)
             .expect(200)
@@ -76,14 +76,14 @@ describe('Book Crud Test', function(){
                 results.body.genre.should.equal('Science Fiction');
                 results.body.title.should.equal('New Book');
                 results.body.author.should.equal('John Doe');
-
+                
                 done();
             })
     })
-
+    
     it('PATCH /books/:bookId', function(done) {
         // console.log('PATCH: '+ bookId)
-
+        
         agent.patch('/api/books/' + bookId)
             .send(bookPatch)
             .expect(200)
@@ -92,34 +92,33 @@ describe('Book Crud Test', function(){
                 results.body.genre.should.equal('Science Fiction');
                 results.body.title.should.equal('New Book');
                 results.body.author.should.equal('John Doe');
-                // results.body.read.should.equal(true);
-                results.body.read.should.equal(false);
-
+                results.body.read.should.equal(true);
+                
                 // console.log(results.body);
                 done();
             })
     })
-
+    
     it('DELETE /books/:bookId', function(done) {
         agent.del('/api/books/' + bookId)
             .expect(200)
             .end(function(err, results){
                 if (err) return console.error('DELETE /books/:bookId (%s)', err);
                 results.body.message.should.equal('Successful delete');
-
+                
                 // console.log(results.body);
                 done();
             })
     })
-
+    
     // afterEach(function(done){
     //   Book.remove().exec();
     //   done();
     // })
-
+    
     after(function(done) {
         Book.remove({}).exec();
         done();
     })
-
+    
 });
